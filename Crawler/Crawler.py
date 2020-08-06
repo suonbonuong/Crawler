56f1c4c0ce8752937c95fa3bf17eb1617c45fe47
ShadowofBQT <39153130+suonbonuong@users.noreply.github.com>
2020-08-06 14:15:18 +0700

    First commit

---
+++ Crawler/Crawler.py
@@ -0,0 +1,18 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
+bsObj = BeautifulSoup(html)
+#print(bsObj)
+nameList = bsObj.findAll("span", {"class":"green"})
+for name in nameList:
+    print(name.get_text())
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
+bsObj = BeautifulSoup(html)
+#print(bsObj)
+nameList = bsObj.findAll("span", {"class":"green"})
+for name in nameList:
+    print(name.get_text())
